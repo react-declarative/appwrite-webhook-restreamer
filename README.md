04c0f004b08bedd0f218dd490a83b10a432f36a6
Petr Tripolsky <tripolskypetr@gmail.com>
2024-07-07 18:49:28 +0500

    publish

---
+++ README.md
@@ -0,0 +1,64 @@
+# appwrite-webhook-restreamer
+
+> Appwrite Server SDK does not support realtime through WebSocket. Due to the limitations of automatic SDK generator, [looks like It would not ever support It](https://appwrite.io/threads/1180136902481952838). I am solved that problem by using [SockJS](https://github.com/sockjs/sockjs-client)
+
+## Contribute
+
+> [!IMPORTANT]
+> There is another project named [react-declarative](https://github.com/react-declarative/react-declarative) build especially for AppWrite Realtime development. **⭐Star** and **💻Fork** It on github will be appreciated
+
+# Setup
+
+1. Start this microservice by using the next command
+
+```bash
+npx -y appwrite-webhook-restreamer
+```
+
+2. Setup [webhook](https://appwrite.io/docs/advanced/platform/webhooks) in the Appwrite instance settings. Use the next POST URL: `http://127.0.0.1:9999/emit`
+
+![appwrite-webhook-settings](./assets/appwrite-webhook-settings.png)
+
+3. Listen for events by using [any SockJS Client](https://github.com/sypbiz/SockJS.NET) with the next url: `http://127.0.0.1:9999/listen`. The following code might be useful
+
+```tsx
+import Socket from "sockjs-client";
+
+import { Subject, singleshot } from "react-declarative";
+
+...
+
+export class RealtimeService {
+
+  private readonly _sockjsSubject = new Subject<object>();
+
+  constructor() {
+    makeObservable(this, {});
+  }
+
+  private _init = singleshot(() => {
+    new Socket("http://127.0.0.1:9999/listen").onmessage = (msg) => {
+      const chunk = JSON.parse(msg.data);
+      const data = {
+        payload: chunk.payload,
+        channels: [],
+        events: chunk.events.split(","),
+        timestamp: chusnk.timestamp,
+      };
+      this._sockjsSubject.next(data);
+    };
+  });
+
+  subscribe = <T extends unknown>(
+    channel: string,
+    callback: (payload: RealtimeResponseEvent<T>) => void,
+  ) => {
+      this._init();
+      return this._sockjsSubject.subscribe((payload) => {
+        if (payload.events.some((event: string) => event.includes(channel))) {
+          callback(payload);
+        }
+      });
+  };
+}
+```
---
+++ index.mjs
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+import { serializeError } from "serialize-error";
+
+import http from "http";
+import fs from "fs";
+
+import bodyParser from "body-parser";
+
+import EventEmitter from "events";
+
+import express from "express";
+import sockjs from "sockjs";
+
+const [port = "9999"] = process.argv.slice(2);
+
+const app = express();
+
+app.use((req, res, next) => {
+  if (req.url === "/listen") {
+    return;
+  }
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.options("*", (req, res) => {
+  res.setHeader(
+    `Access-Control-Allow-Origin`,
+    req.headers.origin || req.hostname || "*",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    req.headers["access-control-request-headers"] || "*",
+  );
+  res.setHeader(`Access-Control-Allow-Methods`, `GET,POST,PUT,PATCH,DELETE`);
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.sendStatus(200);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
+  fs.writeFileSync("./error.txt", JSON.stringify(serializeError(error), null, 2));
+  process.exit(-1);
+});
+
+process.on("unhandledRejection", (error) => {
+  throw error;
+});
+
+const sockjsServer = sockjs.createServer();
+
+const emitter = new EventEmitter();
+
+{
+  const clients = [];
+  sockjsServer.on('connection', (conn) => {
+    clients.push(conn);
+    conn.on('close', () => {
+      const index = clients.indexOf(conn);
+      if (index !== -1) {
+        clients.splice(index, 1);
+      }
+    });
+  });
+  emitter.on('event', (data) => {
+    clients.forEach((client) => {
+      restreamLogger.info(data);
+      client.write(JSON.stringify(data));
+    });
+  });
+}
+
+app.post("/emit", (req, res) => {
+    const userId = req.header("x-appwrite-webhook-user-id");
+    const projectId = req.header("x-appwrite-webhook-project-id");
+    const events = req.header("x-appwrite-webhook-events");
+    const timestamp = Date.now();
+    const payload = req.body;
+    emitter.emit("event", {
+      userId,
+      projectId,
+      events,
+      timestamp,
+      payload,
+    });
+    res.status(200).send("ok");
+});
+
+const server = http
+    .createServer(app)
+    .listen(port, "0.0.0.0")
+    .addListener("listening", () => {
+      console.log(`Restream started: PORT=${port}`);
+    });
+
+sockjsServer.installHandlers(server, { prefix: '/listen', websocket: false });
